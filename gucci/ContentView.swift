6962df9c40e541ddb213066a4d6fe0faba6ee2b3
Francesco Paciello <kekkopacio@gmail.com>
2021-03-02 14:44:14 +0100

    Initial Commit

---
+++ gucci/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  gucci
+//
+//  Created by Kekko Paciello on 02/03/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ gucci/gucciApp.swift
@@ -0,0 +1,17 @@
+//
+//  gucciApp.swift
+//  gucci
+//
+//  Created by Kekko Paciello on 02/03/21.
+//
+
+import SwiftUI
+
+@main
+struct gucciApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
